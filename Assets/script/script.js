853e6b15353163c3444f0fbba34323b1d34be7a0
Roehit2001 <roehitr1@gmail.com>
2021-04-04 12:13:48 +0530

    update

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,55 +1,43 @@
 function visFunction() {
-    document.getElementById("APPimg").style.display = "block";
+    document.getElementById("APPimg").style.display = "contents";
     document.getElementById("EXAMimg").style.display = "none";
-    document.getElementById("PROJimg").style.display = "none";
+    
     document.getElementById("INTimg").style.display = "none";
     document.getElementById("RESimg").style.display = "none";
    
     document.getElementById("APPbtn").style.backgroundColor = "#0077ff";
     document.getElementById("EXAMbtn").style.backgroundColor = "#323335";
-    document.getElementById("PROJbtn").style.backgroundColor = "#323335";
+   
     document.getElementById("INTbtn").style.backgroundColor = "#323335";
     document.getElementById("RESbtn").style.backgroundColor = "#323335";
 }
 
 function visFunction1() {
     document.getElementById("APPimg").style.display = "none";
-    document.getElementById("EXAMimg").style.display = "block";
-    document.getElementById("PROJimg").style.display = "none";
+    document.getElementById("EXAMimg").style.display = "contents";
+    
     document.getElementById("INTimg").style.display = "none";
     document.getElementById("RESimg").style.display = "none";
 
     document.getElementById("APPbtn").style.backgroundColor = "#323335";
     document.getElementById("EXAMbtn").style.backgroundColor = "#0077ff";
-    document.getElementById("PROJbtn").style.backgroundColor = "#323335";
+   
     document.getElementById("INTbtn").style.backgroundColor = "#323335";
     document.getElementById("RESbtn").style.backgroundColor = "#323335";
 }
 
-function visFunction2() {
-    document.getElementById("APPimg").style.display = "none";
-    document.getElementById("EXAMimg").style.display = "none";
-    document.getElementById("PROJimg").style.display = "block";
-    document.getElementById("INTimg").style.display = "none";
-    document.getElementById("RESimg").style.display = "none";
 
-    document.getElementById("APPbtn").style.backgroundColor = "#323335";
-    document.getElementById("EXAMbtn").style.backgroundColor = "#323335";
-    document.getElementById("PROJbtn").style.backgroundColor = "#0077ff";
-    document.getElementById("INTbtn").style.backgroundColor = "#323335";
-    document.getElementById("RESbtn").style.backgroundColor = "#323335";
-}
 
 function visFunction3() {
     document.getElementById("APPimg").style.display = "none";
     document.getElementById("EXAMimg").style.display = "none";
-    document.getElementById("PROJimg").style.display = "none";
-    document.getElementById("INTimg").style.display = "block";
+    
+    document.getElementById("INTimg").style.display = "contents";
     document.getElementById("RESimg").style.display = "none";
 
     document.getElementById("APPbtn").style.backgroundColor = "#323335";
     document.getElementById("EXAMbtn").style.backgroundColor = "#323335";
-    document.getElementById("PROJbtn").style.backgroundColor = "#323335";
+   
     document.getElementById("INTbtn").style.backgroundColor = "#0077ff";
     document.getElementById("RESbtn").style.backgroundColor = "#323335";
 }
@@ -60,13 +48,13 @@ function visFunction3() {
 function visFunction4() {
     document.getElementById("APPimg").style.display = "none";
     document.getElementById("EXAMimg").style.display = "none";
-    document.getElementById("PROJimg").style.display = "none";
+    
     document.getElementById("INTimg").style.display = "none";
-    document.getElementById("RESimg").style.display = "block";
+    document.getElementById("RESimg").style.display = "contents";
 
     document.getElementById("APPbtn").style.backgroundColor = "#323335";
     document.getElementById("EXAMbtn").style.backgroundColor = "#323335";
-    document.getElementById("PROJbtn").style.backgroundColor = "#323335";
+   
     document.getElementById("INTbtn").style.backgroundColor = "#323335";
     document.getElementById("RESbtn").style.backgroundColor = "#0077ff";
 }
